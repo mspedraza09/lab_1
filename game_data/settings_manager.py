79b339ad7eb3089bcc577e1425534c63a4d2ad6e
mspedraza9 <sofia.901.maria@gmail.com>
2026-03-22 22:23:35 -0500

    actualización de game_data
    Se creó y codificó los archivos de game_data

---
+++ game_data/settings_manager.py
@@ -0,0 +1,17 @@
+from game.persistence.Hash_table import StorageEngine
+
+class SettingVolumen:
+    def __init__(self):
+        self.store = StorageEngine()
+    
+
+    def save_setting_volumen(self, volume_level, data):
+        self.store.save("Setting", f"settings:global",{"volume": volume_level} )
+    
+    
+    def get_volume(self):
+        settings = self.store.get("settings:global")
+        
+        if settings is None:
+            return 50 
+        return settings.get("volume", 50)
